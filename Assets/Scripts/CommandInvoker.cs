ffcad3054c7ecc448927c3e0fe252352f51b81f4
tayzar-v98 <tayzar@6ck.jp>
2021-05-10 17:04:41 +0900

    Added command invoker

---
+++ Assets/Scripts/CommandInvoker.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker
+{
+    private readonly List<ICommand> _commandHistory = new List<ICommand>();
+    private static int _counter = 0;
+
+    public void AddCommand(ICommand command)
+    {
+        if (_counter < _commandHistory.Count)
+        {
+            _commandHistory.RemoveRange(_counter, _commandHistory.Count - _counter);
+            _counter = _commandHistory.Count;
+        }
+
+        _commandHistory.Add(command);
+        _counter++;
+    }
+
+    public void Execute()
+    {
+        if (_counter == 0)
+        {
+            return;
+        }
+
+        for (var i = 0; i < _counter; i++)
+        {
+            _commandHistory[i].Execute();
+        }
+    }
+
+    public Boolean TryUndoCommand()
+    {
+        if (_counter == 0)
+        {
+            return false;
+        }
+
+        _counter--;
+        return true;
+    }
+}
